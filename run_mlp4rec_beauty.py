ea13026af3200a5ac64af418fc9e3dffa283fc7f
Phuc <tranthanhphuc042@gmail.com>
2024-11-10 10:47:00 +0000

    Add run for beauty

---
+++ run_mlp4rec_beauty.py
@@ -0,0 +1,5 @@
+from recbole.quick_start import run_recbole
+
+run_recbole(
+    model="MLP4Rec",
+    dataset="beauty",config_dict={"epochs":100,"neg_sampling":None,"train_batch_size":256,"eval_batch_size":512,"hidden_dropout_prob":0.5,"learning_rate":0.0001,"n_layers":4,"show_progress":True,"hidden_size":128,"seed":2022,"selected_features":['sales_rank', 'categories', 'price', 'brand']})
